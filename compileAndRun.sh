79d3d932a00b9836b9094ce505715aa1db678624
IdioticLoser55 <idioticloser55@gmail.com>
2021-10-20 22:08:15 +0100

    missed a couple libraries

--- compileAndRun.sh
+++ compileAndRun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-javac -cp .:lib/Java-WebSocket-1.5.2.jar -d build src/main/java/de/mat2095/my_slither/*.java
+javac -cp .:lib/Java-WebSocket-1.5.2.jar:lib/darcula.jar:lib/slf4j-api-1.7.32.jar:lib/slf4j-simple-1.7.32.jar -d build src/main/java/de/mat2095/my_slither/*.java
 if [ $? -eq 0 ]; then
-    java -cp build de/mat2095/my_slither/Main
+    java -cp .:lib/Java-WebSocket-1.5.2.jar:lib/slf4j-api-1.7.32.jar:lib/slf4j-simple-1.7.32.jar:build de/mat2095/my_slither/Main
 fi
 
